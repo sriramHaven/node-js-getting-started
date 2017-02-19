65e10bef38a53692c76103fcff60b8627e19dae0
Manoharan, Sriram <sriram@havenlife.com>
2017-02-19 14:56:53 -0500

    messenger

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
+const bodyParser = require('body-parser')
+const request = require('request')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -21,3 +23,23 @@ app.get('/cool', function(request, response) {
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
+
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+    res.send('Hello world, I am a chat bot')
+})
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'go_haven') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
